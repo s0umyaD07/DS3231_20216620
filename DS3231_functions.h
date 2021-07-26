e9ac8daf2a2a8e3b4094a339c0d10b6eb9a6fc95
s0umyaD07 <82377301+s0umyaD07@users.noreply.github.com>
2021-07-26 10:00:09 -0700

    Create DS3231_functions.h

---
+++ DS3231_functions.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+
+class DS3231_functions
+{
+
+    public:
+
+    void DS3231_functions(){
+
+
+    }
+
+    int bcd_Dec(char b)
+    {
+
+    }
+
+    void Read_Display_Time_Date()
+    {
+
+    
+    }
+
+    string Read_Display_Temp()
+
+    {
+
+
+    }
+
+    void Set_Time_Date() {
+
+
+    }
+
+    void Set_Alarm (){
+
+
+    }
+
+}
