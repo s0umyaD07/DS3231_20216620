923e021a2c35e33ce24265183af48d51c39849b2
s0umyaD07 <82377301+s0umyaD07@users.noreply.github.com>
2021-07-27 22:08:17 +0530

    Update DS3231_main.cpp
    ok

--- DS3231_main.cpp
+++ DS3231_main.cpp
@@ -1,14 +1,16 @@
+
 #include<iostream>
 #include "DS3231_functions.h"
 
-using namespace std;
+using namespace std ;
 
 int main()
 {
    
 DS3231_functions d;
 
-d.Read_Display_Time_Date;
 
 
+d.Read_Display_Time_Date();
+return 0;
 }
