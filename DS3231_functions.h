977df338f4b2b3f193a9223f31ff46d8f4cb466e
s0umyaD07 <82377301+s0umyaD07@users.noreply.github.com>
2021-07-27 22:51:16 -0700

    Update DS3231_functions.h

--- DS3231_functions.h
+++ DS3231_functions.h
@@ -1,25 +1,34 @@
+  
 #include <iostream>
 #include <string>
 using namespace std ;
 
 class DS3231_functions
-{
+{   private:
+	    unsigned int bus;
+	    unsigned int device;
+	    int file;
+
 
     public:
 
-    DS3231_functions();
+    DS3231_functions(unsigned int bus, unsigned int device);
+	virtual int open();
+	virtual int write(unsigned char value);
+	virtual unsigned char read_Register(unsigned int registerAddress);
+    virtual int write_Register(unsigned int registerAddress, unsigned int value);
+    virtual void close();
+    virtual int Read_Display_Time_Date();
+    virtual int Read_Display_Temp();
+    virtual int Set_Time_Date(unsigned int Date,unsigned int Month,unsigned int Year,unsigned int Hour,unsigned int Minute,unsigned int Second ) ;
+    virtual int Set_Alarm1(unsigned int Date,unsigned int Hour,unsigned int Minute,unsigned int Second );
+    virtual int Read_Alarm1();
 
-    
+      virtual int Set_Alarm2(unsigned int Day,unsigned int Hour,unsigned int Minute );
+    virtual int Read_Alarm2();
+    virtual int Interrupt_SqwGenerator();
 
-      int Read_Display_Time_Date();
-    
 
-    void Read_Display_Temp();
 
-    
-
-    void Set_Time_Date() ;
-
-    void Set_Alarm ();
+    virtual ~DS3231_functions();
 };
-
